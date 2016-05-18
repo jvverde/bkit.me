23f945fc2785c6688bb7c72f637322029af25793
Isidro Vila Verde <jvverde@gmail.com>
2016-05-18 19:43:10 +0100

    init

--- bk-init.pl
+++ bk-init.pl
@@ -7,99 +7,41 @@ use JSON;
 use Data::Dumper;
 use Win32;
 
-
 ($\,$,) = ("\n","\t");
 my $json = (new JSON)->utf8->pretty;
 
-print Win32::GetOSName();
-my ($string, $major, $minor, $build, $id, $spmajor, $spminor, $suitemask, $producttype) = Win32::GetOSVersion();
-print $string, $major, $minor, $build, $id, $spmajor, $spminor, $suitemask, $producttype;
-print Win32::GetOSDisplayName();
-print Win32::GetArchName();
-print Win32::GetChipName();
-print Win32::GetOEMCP();
-print Win32::NodeName();
-print Win32::DomainName();
-print Win32::GetProductInfo($major, $minor, $spmajor, $spminor);
-
-
-
-exit;
-
-sub saveData{
-  my ($file,$data) = @_;
-  open my $fhv, ">$file" or (warn "Cannot save info to $file" and return undef);
-  print $fhv $data; 
-  close $fhv;
-  return $data;
-} 
-sub drive2DevId{
-  my ($drive,$lsv) = @_;
-  my $volumes = $json->decode($lsv) or die "Not json:$!";
-  my ($volume) = grep{defined $_->{DriveLetter} and uc $_->{DriveLetter} eq "$drive:"} @$volumes;
-  return $volume->{DeviceID};
-}
-sub getShadowCopies{
-  my ($volume,$lsh) = @_;
-  my $shadows = $json->decode($lsh) or die "Not json:$!";
-  return undef unless defined $shadows->{InstallDate} and defined $shadows->{DeviceObject} and defined $shadows->{VolumeName};
-  my $volumes = $shadows->{VolumeName};
-  return {map {
-    $shadows->{InstallDate}->[$_] => {
-      volume => $shadows->{DeviceObject}->[$_]
-      ,id => $shadows->{ID}->[$_]
-    }
-  } grep {defined $volumes->[$_] and $volumes->[$_] eq $volume} 0..$#{$volumes}};
-}
-
-
 my $perl = which 'perl';
-my $vssadmin = which 'vssadmin';
 my $cd = dirname abs_path $0;
 my $rsync = "$cd\\cygwin\\rsync.exe";
 $rsync = which 'rsync' or die "Cannot find rsync $rsync" unless -e $rsync;
-my $subinacl = (which 'subinacl') || "$cd\\3rd-party\\subinacl\\subinacl.exe";
 
-my $dir = shift or die 'You must specify a directory';
-my ($drive,$path) = ($dir =~ /^([a-z]):(.*)$/i) or die 'You must include drive letter in directory';
-$drive = uc $drive;
-$path =~ s/^[^\\]?/\\$&/; #garante um backslash no inicio do path
-$path =~ s/[\\]/\//g;    #dos->unix 
+my $osversion = [Win32::GetOSVersion()];
+my $sysInfo = {
+  version => $osversion
+  ,displayName => Win32::GetOSDisplayName()
+  ,arch => Win32::GetArchName()
+  ,chip => Win32::GetChipName()
+  ,oem => Win32::GetOEMCP()
+  ,nodeName => Win32::NodeName()
+  ,domainName => Win32::DomainName()
+  ,product => Win32::GetProductInfo($osversion->[1], $osversion->[2], $osversion->[5], $osversion->[6])
+  ,osName => ''.Win32::GetOSName() .''
+};
 
-my $bkitDir = '.bkit.me';
-my $bkit = "$drive:\\$bkitDir";
--d $bkit or mkdir $bkit;
-my ($logs,$perms,$vols) = map {-d $_ or mkdir $_; $_} map {"$bkit\\$_"} qw(logs perms vols);
+my $sysDir = "$cd\\sysInfo";
+-d $sysDir or mkdir $sysDir;
+my $wmiFile = "$sysDir\\wmi.info"; 
+my $sysFile = "$sysDir\\sys.info"; 
+system qq|$perl $cd\\getinfo.pl > $wmiFile| and die "Error while getting wmi info:$? ($!)";
+open my $h, ">$sysFile" or die "Cannot open $sysFile: $!"; 
+print $h $json->encode($sysInfo);
+close $h;
+exit;
 
-my $acls = "$perms\\acls.txt";
+my $node = $sysInfo->{nodeName};
+# open my $handler, "|-"
+  # ,qq|${rsync} -rltvvvhR --chmod=ugo=rwX --inplace --stats|
+  # .qq| /proc/sys/Device/${shcN}/${bkitDir}/../.${path} me\@10.1.2.6::bkit.me/${drive}/|
+  # .qq| 2>${bkit}\\logs\\err.txt >${bkit}\\logs\\logs.txt|;
+# print $handler "me\n\n";
 
-my $mtime = (stat $acls)[9] if -e $acls;
-$mtime //= 0;
-print qx|$subinacl /noverbose /output=$acls /subdirectories $drive:\\| 
-  if -e $subinacl and (time - $mtime) > 3600*24*8;#8 day
-
-my $lsv = qx|$perl $cd\\getvol.pl| or die "Error code $? ($!)";
-saveData "$vols\\volumes.txt", $lsv;
-
-my $devId = drive2DevId $drive, $lsv or die "Cannot get DeviceId for drive $drive:$!";
-system qq|$perl $cd\\csc.pl $drive| and die "Cannot create shadow copy, Error value: $? ($!)";
-my $lsh = qx|$perl $cd\\lsh.pl| or die "Error code $? ($!)";
-my $cvss = getShadowCopies $devId, $lsh;
-die 'Cannot get shadow Copies' unless defined $cvss and scalar %$cvss;
-my $lastVssKey = pop @{[sort keys %$cvss]};
-my $cur = $cvss->{$lastVssKey}->{volume};
-if (defined $cur){
-  my ($shcN) = $cur =~ /(HarddiskVolumeShadowCopy\d+)/;
-  open my $handler, "|-"
-    ,qq|${rsync} -rltvvvhR --chmod=ugo=rwX --inplace --delete-after --stats --exclude-from=$cd\\conf\\excludes.txt|
-    .qq| /proc/sys/Device/${shcN}/${bkitDir}/../.${path} me\@10.1.2.6::meatfeup/${drive}/|
-    .qq| 2>${bkit}\\logs\\err.txt >${bkit}\\logs\\logs.txt|;
-  print $handler "me\n\n";
-}
-
-END {
-  if (defined $lastVssKey){
-    my $last = $cvss->{$lastVssKey}->{id};
-    print qx|$vssadmin Delete Shadows /Shadow=$last /Quiet|;
-  }
-}
---
+++ getinfo.pl
@@ -0,0 +1,33 @@
+use strict;
+use Win32::OLE('in');use Data::Dumper;
+use JSON;
+
+my $json = (new JSON)->utf8->pretty;
+use constant wbemFlagReturnImmediately => 0x10;
+use constant wbemFlagForwardOnly => 0x20;
+
+my $objWMIService = Win32::OLE->GetObject("winmgmts:\\\\.\\root\\CIMV2") or die "WMI connection failed.\n";
+sub get{
+  my ($class, $attr) = @_;
+  my $colItems = $objWMIService->ExecQuery("SELECT * FROM $class", "WQL", wbemFlagReturnImmediately | wbemFlagForwardOnly);
+  my $result = [];
+        
+
+  foreach my $objItem (in $colItems) {
+    push @$result, {map {$_ => $objItem->{$_}} @$attr};
+  }
+  return $result->[0];
+}
+my %obj = (
+  Win32_OperatingSystem => [qw|BootDevice BuildNumber BuildType Caption CodeSet CSName Locale Name 
+    OperatingSystemSKU OSLanguage OSProductSuite ProductType SerialNumber ServicePackMajorVersion 
+    ServicePackMinorVersion SystemDevice SystemDirectory SystemDrive Version WindowsDirectory
+  |]
+  ,Win32_ComputerSystemProduct => [qw|IdentifyingNumber Name SKUNumber Vendor Version UUID|]
+  ,Win32_ComputerSystem => [qw|DNSHostName Domain DomainRole Manufacturer Model Name SystemSKUNumber Workgroup|]
+  ,Win32_BIOS => [qw|BIOSVersion BuildNumber Manufacturer Name PrimaryBIOS SerialNumber SMBIOSBIOSVersion 
+    SMBIOSMajorVersion SMBIOSMinorVersion SMBIOSPresent SoftwareElementState SystemBiosMajorVersion SystemBiosMinorVersion Version
+  |]
+);
+my $result = {map {$_ => get $_, $obj{$_}} keys %obj}; 
+print $json->encode($result);
